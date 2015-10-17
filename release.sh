51d6ab98e37a80e5d83e8b785fca677342656c90
shwld <shwld@outlook.com>
2015-10-17 20:08:19 +0900

    add options to mac OS X release shell

--- release.sh
+++ release.sh
@@ -1,5 +1,22 @@
 (cd electron;npm install)
 gulp electron
+
+while getopts l:d OPT
+do
+    case $OPT in
+        l)
+            case $OPTARG in
+                "en")
+                    cp ./electron/main-en_US.js ./build/electron/main.js
+                    ;;
+            esac
+            ;;
+        d)
+            cp ./electron/main-debug.js ./build/electron/main.js
+            ;;
+    esac
+done
+
 #gulp minify
 electron-packager ./build/electron MdNote --overwrite --platform=darwin --arch=x64 --version=0.31.2 --asar=true --icon=./build/electron/assets/icon.icns
-cp ./LICENSE ./MdNote-win32-x64/.
+cp ./LICENSE ./MdNote-darwin-x64/.
